1b02b6baab35f3ccb1fa83bb2dc9efe7d5d5b7e9
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-26 15:24:11 -0300

    chore: rename application

--- src/main/java/com/weg/biblioteca/GestaoEscolarApplication.java
+++ src/main/java/com/weg/biblioteca/GestaoEscolarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BibliotecaApplication {
+public class GestaoEscolarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaApplication.class, args);
+		SpringApplication.run(GestaoEscolarApplication.class, args);
 	}
 
 }
